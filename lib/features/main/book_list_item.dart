149b32700fcbfa11a9e33bbc525d7b546d3227fa
duongdt3 <doanthanhduong11031990@gmail.com>
2020-04-20 16:30:35 +0700

    use cache image lib, change padding book list

--- lib/features/main/book_list_item.dart
+++ lib/features/main/book_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:booklibrary2020/features/main/book_detail.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:booklibrary2020/data/models/book.dart';
 import 'package:flutter/widgets.dart';
@@ -18,21 +19,30 @@ class BookListItem extends StatelessWidget {
         Navigator.push(context, MaterialPageRoute(builder: (context) => BookDetail(bookEntity: item)));
       },
       child: Container(
-        margin: EdgeInsets.all(8.0),
-        color: Colors.white,
+        margin: EdgeInsets.fromLTRB(28, 8, 28, 8),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(5.0),
-              child: Image.network(
-                item.thumbUrl,
-                width: 80,
-                height: 80,
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  border: Border.all(color: Colors.grey, width: 0.3)),
+              width: 80,
+              height: 80,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5.0),
+                child: CachedNetworkImage(
+                  fit: BoxFit.fill,
+                  errorWidget: (context, url, error) =>
+                      Icon(Icons.error_outline, color: Colors.red, size: 28,),
+                  imageUrl: item.thumbUrl,
+                  width: 80,
+                  height: 80,
+                ),
               ),
             ),
-            SizedBox(width: 5,),
+            SizedBox(width: 16,),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -42,24 +52,24 @@ class BookListItem extends StatelessWidget {
                   Text(
                     item.categoryName,
                     style: TextStyle(
-                        fontSize: 15,
+                        fontSize: 13,
                         color: Colors.black
                     ),
                   ),
-                  SizedBox(height: 5,),
+                  SizedBox(height: 6),
                   Text(
                     item.name,
                     style: TextStyle(
-                      fontSize: 20,
+                      fontSize: 16,
                       color: Colors.black,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 5,),
+                  SizedBox(height: 10),
                   Text(
                     item.author,
                     style: TextStyle(
-                        fontSize: 15,
+                        fontSize: 13,
                         color: Color.fromRGBO(135, 135, 135, 1.0)
                     ),
                   )
